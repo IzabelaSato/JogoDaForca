af9d5fad9917b55b317c9d649d8988265438cf8e
IzabelaSato <izataiette@gmail.com>
2022-06-06 18:17:56 -0300

    criando multiplos dicionarios

--- JogoDaForca/lib/src/main/java/palavras/Dictionary.java
+++ JogoDaForca/lib/src/main/java/palavras/Dictionary.java
@@ -7,42 +7,17 @@ import java.util.Scanner;
 import jogoforca.GameException;
 import jogoforca.utils.RandomUtils;
 
-
-public class Dictionary {
-
-	private static final String FILE_NAME = "dicionario.txt";
+public abstract class Dictionary {
 
 	public static Dictionary instance;
 
-	private List<String> words = new ArrayList<>();
-
-	private Dictionary() {
-		load();
-	}
-
 	public static Dictionary getInstance() {
-		if (instance == null) {
-			instance = new Dictionary();
-		}
+	//	if (instance == null) {
+	//		instance = new Dictionary();
+	//	}
 		return instance;
 	}
 	
-	private void load() {
-		try (Scanner scanner = 
-				new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
 
-			while (scanner.hasNextLine()) {
-				String word = scanner.nextLine().trim();
-				words.add(word);
-			}
-			if (words.size() == 0) {
-				throw new GameException("A lista de palavras não pode ser vazia");
-			}
-		}
+	public abstract word nextWord();
 	}
-
-	public word nextWord() {
-		int pos = RandomUtils.newRandomNumber(0, words.size());
-		return new word(words.get(pos));
-	}
-}
